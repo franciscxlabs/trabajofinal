e46926835169b59c00dd4fa145ade09b29b8480a
franciscocr <inalambrico@gmail.com>
2015-11-19 00:55:13 -0300

    inciando2

--- src/trabajofinal1215/Trabajofinal1215.java
+++ src/trabajofinal1215/Trabajofinal1215.java
@@ -6,7 +6,9 @@ public class Trabajofinal1215
     public static void main(String[] args) 
     {
         
-
+        System.out.println("hola");
+        System.out.println("hola");
+        /////////////////////////////////////////////
 
 
     }
