6e22e09434598d9e66bca7846285d0c5ee2602b0
criscont <cris.contrerasjara@gmail.com>
2015-11-20 20:45:30 -0300

    creación de tablas
    las descomentan y copy-pasta

--- src/trabajofinal1215/nueva.java
+++ src/trabajofinal1215/nueva.java
@@ -11,7 +11,51 @@ package trabajofinal1215;
  */
 public class nueva {
     
- int p =0;
+ // para la creacion de las trablas:
+//create table cliente (
+//codigo_cli int,
+//nombre varchar (20),
+//apellido_p (20),
+//apellido_m (20),
+//primary key (codigo_cli),
+//);
+//
+//create table auto (
+//codigo_au int,
+//marca varchar (20),
+//modelo varchar (20),
+//color varchar (20),
+//precio int,
+//primary key (codigo_au),
+//);
+//
+//create table factura (
+//codigo_fac int,
+//fecha_compra date,
+//cliente_cod int,
+//auto_cod int,
+//estado varchar (15),
+//primary key (codigo_fac),
+//foreign key (cliente_cod) references cliente (codigo_cli),
+//foreign key (auto_cod) references auto (codigo_au),
+//);
+//
+//create table detalle_factura (
+//codigo_det int,
+//compra int,
+//accesorio int,
+//estado varchar (15),
+//primary key (codigo_det),
+//foreign key (compra) references factura (codigo_fac),
+//foreign key (accesorio) references accesorio (codigo_acc),
+//);
+//
+//create table accesorio (
+//codigo_acc int,
+//descripcion varchar (30),
+//precio int,
+//primary key (codigo_acc)
+//);
 
 }
     
