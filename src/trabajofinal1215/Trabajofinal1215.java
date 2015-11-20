98a90af5e84786aae6ab8c8f274f316e5534bc3e
criscont <cris.contrerasjara@gmail.com>
2015-11-20 20:25:10 -0300

    agregue hola fucking mundo

--- src/trabajofinal1215/Trabajofinal1215.java
+++ src/trabajofinal1215/Trabajofinal1215.java
@@ -8,7 +8,7 @@ public class Trabajofinal1215
         
         System.out.println("hola");
         System.out.println("hola");
-        System.out.println("hola");
+        System.out.println("hola mundo");
         System.out.println("12334346457");
         /////////////////////////////////////////////
         int i;
