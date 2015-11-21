3b6ae4183a412c70738e17e5aa66b403048221f7
17034556-8 <17034556-8@pmt_lab0125.inacap.cl>
2015-11-20 21:02:21 -0300

    asdasda

---
+++ src/otro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 17034556-8
+ */
+public class otro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=0;
+        for(i=0;i<10;i++){
+            System.out.println("hola"+i);
+        }
+    }
+    
+}
